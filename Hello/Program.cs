c1ae8aefdd92102d973f4727b22012b75da0beac
Piotr Wójcik <krakownh1@gmail.com>
2020-02-24 22:22:02 +0100

    Add project files.

---
+++ Hello/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = args[0];
+            Console.WriteLine("Witaj" + name);
+            //foreach(var item in args)
+            //{
+            //    Console.WriteLine("Witaj " + item);
+            //}
+        }
+    }
+}
